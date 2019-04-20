023189bb17eb2a816fab46c84d98edcbe43faf0f
NieLamu <mxmxlty@gmail.com>
2019-04-20 12:16:26 +0800

    build: update pre commit hooks

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # https://gist.github.com/dahjelle/8ddedf0aebd488208a9a7c829f19b9e8
+# https://esappear.github.io/2018/06/28/pre-commit/
 
-for file in $(git diff --cached --name-only | grep -E '\.(js|jsx)$')
+for file in $(git diff --cached --name-only --diff-filter ACMR | grep -E '\.(js|jsx)$')
 do
   git show ":$file" | node_modules/.bin/eslint --stdin --stdin-filename "$file" # we only want to lint the staged changes, not any un-staged changes
   if [ $? -ne 0 ]; then
