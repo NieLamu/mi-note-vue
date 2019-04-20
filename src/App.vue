2942cbfb14e1849eb8c11145131b513974e9ce00
NieLamu <mxmxlty@gmail.com>
2019-04-20 12:18:04 +0800

    feat: add framework7 kitchen-sick vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,60 @@
 <template>
   <!-- Main Framework7 App component where we pass Framework7 params -->
-  <f7-app :params="f7params">
+  <f7-app :params="f7Params">
     <f7-statusbar></f7-statusbar>
+    <f7-panel left cover resizable>
+      <f7-view url="/panel-left/" links-view=".view-main"></f7-view>
+    </f7-panel>
+    <f7-panel right reveal resizable>
+      <f7-view url="/panel-right/"></f7-view>
+    </f7-panel>
     <!-- initial page is specified in routes.js -->
-    <f7-view main url="/"></f7-view>
+    <f7-view
+      url="/"
+      :main="true"
+      class="safe-areas"
+      :master-detail-breakpoint="800"
+    ></f7-view>
   </f7-app>
 </template>
 
 <script>
-import f7params from './f7params'
+import { f7App, f7Panel, f7View, f7Statusbar } from 'framework7-vue'
+import routes from './routes'
 
 export default {
+  components: {
+    f7App,
+    f7Panel,
+    f7View,
+    f7Statusbar
+  },
   data () {
+    // Demo Theme
+    let theme = 'auto'
+    if (document.location.search.indexOf('theme=') >= 0) {
+      theme = document.location.search.split('theme=')[1].split('&')[0]
+    }
+
     return {
-      // app params
-      f7params
+      f7Params: {
+        name: 'MI NOTE',
+        id: 'io.github.NieLamu',
+        theme,
+        routes,
+        popup: {
+          closeOnEscape: true
+        },
+        sheet: {
+          closeOnEscape: true
+        },
+        popover: {
+          closeOnEscape: true
+        },
+        actions: {
+          closeOnEscape: true
+        }
+      }
     }
   },
   mounted () {
@@ -27,11 +67,6 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import "./assets/css/app.css";
 </style>
+>
--- src/main.js
+++ src/main.js
@@ -7,15 +7,18 @@ import VueAxios from 'vue-axios'
 // Framework7 Library CSS Bundle
 import 'framework7/css/framework7.bundle.min.css'
 // Import Framework7 Bundle
-import Framework7 from 'framework7/framework7.esm.bundle.js'
-// Import Framework7-Vue with all components
-import Framework7Vue from 'framework7-vue/framework7-vue.esm.bundle.js'
+import Framework7 from 'framework7/framework7.esm.bundle'
+// // Import Framework7-Vue with all components
+// import Framework7Vue from 'framework7-vue/framework7-vue.esm.bundle'
+// Import Framework7-Vue, import components you need in your components
+import Framework7Vue from 'framework7-vue'
 // Init plugin and register all components
 Framework7.use(Framework7Vue)
 
 Vue.config.productionTip = true
 Vue.use(VueAxios, axios)
 
+// Init Vue App
 new Vue({
   store,
   render: h => h(App)
