0eec597b4f220635559921e39b0a04900ead77d7
NieLamu <mxmxlty@gmail.com>
2019-05-05 15:49:27 +0800

    build: add runtime for compile

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   // https://cli.vuejs.org/zh/config/#%E7%9B%AE%E6%A0%87%E6%B5%8F%E8%A7%88%E5%99%A8
   publicPath: '',
-  outputDir: 'www'
+  outputDir: 'www',
+  // https://cli.vuejs.org/zh/config/#runtimecompiler
+  runtimeCompiler: true
 }
